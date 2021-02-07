10df166d1627a36512d0375cb860ccda8ae9461b
Sakshat <30901276+sakshatshinde@users.noreply.github.com>
2021-02-07 22:25:19 +0530

    Retrive values from config

--- aniwei_config.py
+++ aniwei_config.py
@@ -1,4 +1,5 @@
 from configparser import ConfigParser
+from colorama import init, Fore, Back, Style
 
 # Manipulating the Config file for AnimeWei
 def manipulate_config(key:str, value:str):
@@ -9,3 +10,19 @@ def manipulate_config(key:str, value:str):
 
     with open('config.ini', 'w') as f:  #f is an instance of opened File
         config.write(f)
+
+# Retrieving values from config.ini
+def get_config_values(section='main', key=None):
+    config = ConfigParser()
+    config.read('config.ini')
+
+    if key is not None:
+        # print(config[section][key])
+        return config[section][key]
+    
+    if key is None:
+        raise ValueError(Fore.RED + Back.WHITE + 
+                        'Enter a key to retrieve a val from config.ini'
+                        + Style.RESET_ALL)
+
+# get_config_values('main')
